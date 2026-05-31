11ff2e69eaf1f40a693ecd1e7adc538e1a475422
Rachmanullah <rachmanullah1@gmail.com>
2026-05-31 14:46:47 +0700

    First commit

---
+++ src/main/java/com/example/ncrsystem/ncrsystem/NcrsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncrsystem.ncrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcrsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ncrsystem/ncrsystem/NcrsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncrsystem.ncrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcrsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
